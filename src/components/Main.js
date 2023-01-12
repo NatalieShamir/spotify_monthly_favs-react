f86a26006b3ca95693b12665f89cd8a4b7e6cb2e
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-01-12 11:57:04 +0200

    add two more Track components to JSX

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,24 @@ import Track from "./Track";
 export default function Main() {
   return (
     <ul className="tracks__list">
-      <Track />
+      <Track
+        index="1"
+        albumImageUrl="https://i.scdn.co/image/ab67616d00004851bd2feaef7519dbfbc402201e"
+        artist="The Wiggles"
+        title="Hot Potato"
+      />
+      <Track
+        index="2"
+        albumImageUrl="https://i.scdn.co/image/ab67616d00004851318443aab3531a0558e79a4d"
+        artist="Taylor Swift"
+        title="All Too Well (Taylor's Version)"
+      />
+      <Track
+        index="3"
+        albumImageUrl="https://i.scdn.co/image/ab67616d0000485107823ee6237208c835802663"
+        artist="Arctic Monkeys"
+        title="The Car"
+      />
     </ul>
   );
 }
