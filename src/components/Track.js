309f9ce59b62b3753f421a285a00190d836bd8e8
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-01-12 11:55:45 +0200

    change span tags contents from static data to dynamic

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,16 +1,17 @@
-export default function Track() {
+export default function Track(props) {
   return (
     <a className="track__item">
-      <span className="track__order-number">1</span>
+      <span className="track__order-number">{props.index}</span>
       <span className="track__info">
         <span
           className="track__image"
           title="cover image of the album featuring the track or an image of the single cover if the track was released as a single"
+          style={{ backgroundImage: `url(${props.albumImageUrl})` }}
         ></span>
         <span className="track__summary">
-          <span className="track__artist">The Wiggles</span>
+          <span className="track__artist">{props.artist}</span>
           <span className="track__separator">●</span>
-          <span className="track__title">Hot Potato</span>
+          <span className="track__title">{props.title}</span>
         </span>
       </span>
     </a>
