d2d4636b3993bdab8cd00d9e73ce93977c27d960
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-01-15 13:44:59 +0200

    replace JSX with TrackList component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,4 @@
-import Track from "./Track";
-
+import TrackList from "./TrackList";
 export default function Main() {
-  return (
-    <ul className="tracks__list">
-      <Track
-        index="1"
-        albumImageUrl="https://i.scdn.co/image/ab67616d00004851bd2feaef7519dbfbc402201e"
-        artist="The Wiggles"
-        title="Hot Potato"
-      />
-      <Track
-        index="2"
-        albumImageUrl="https://i.scdn.co/image/ab67616d00004851318443aab3531a0558e79a4d"
-        artist="Taylor Swift"
-        title="All Too Well (Taylor's Version)"
-      />
-      <Track
-        index="3"
-        albumImageUrl="https://i.scdn.co/image/ab67616d0000485107823ee6237208c835802663"
-        artist="Arctic Monkeys"
-        title="The Car"
-      />
-    </ul>
-  );
+  return <TrackList />;
 }
