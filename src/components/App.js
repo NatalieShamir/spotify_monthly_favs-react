44d7dd00dfd2507122ebc0024551541637a83bb7
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-01-22 12:06:30 +0200

    update const CLIENT_ID value and add useEffect() to check for the token

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Main from "./Main";
 import { api } from "../utils/Api";
 
 export default function App() {
-  const CLIENT_ID = "+++++++++++++++++++++++++++++"
+  const CLIENT_ID = "fd4f7c5a262e4313931a6038a17af7bc"
   const REDIRECT_URI = "http://localhost:3000"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
@@ -11,6 +11,19 @@ export default function App() {
   const [token, setToken] = useState("");
 
   const [tracks, setTracks] = React.useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+      setToken(token)
+    }
+  }, [])
 
   useEffect(() => {
     api
@@ -26,7 +39,7 @@ export default function App() {
     <div className="page">
       <header className="header">
         <h1>Spotify Monthly Favorites</h1>
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_url=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
           to Spotify</a>
       </header>
       <Main tracks={tracks} />
