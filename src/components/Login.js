b81d2abebfde0d0c90584b4b82d11f10ae7e5474
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-01-25 13:35:46 +0200

    move useEffect() that handles token from Login to TrackList

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-export default function Login(props) {
+export default function Login() {
     const CLIENT_ID = "fd4f7c5a262e4313931a6038a17af7bc"
     const REDIRECT_URI = "http://localhost:3000/track-list"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
 
-    useEffect(() => {
-        const hash = window.location.hash
-        let token = window.localStorage.getItem("spotify-monthly-favorites-token")
-
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-            window.location.hash = ""
-            window.localStorage.setItem("spotify-monthly-favorites-token", token)
-            props.setToken(token);
-        }
-    }, []);
 
     return (
         <div className="login-page">
--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Track from "./Track";
 
-export default function TrackList(props) {
+export default function TrackList({ setToken, tracks }) {
 
-  const trackComponents = props.tracks.map((track) => <Track {...track} key={track.id} />);
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("spotify-monthly-favorites-token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("spotify-monthly-favorites-token", token)
+      setToken(token);
+    }
+  }, []);
+
+  const trackComponents = tracks.map((track) => <Track {...track} key={track.id} />);
+
   return (
     <ul className="tracks__list">
       {React.Children.toArray(trackComponents)}
