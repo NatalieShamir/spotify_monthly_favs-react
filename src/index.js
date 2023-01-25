0db5b6ea0537f275ed9312e44b58b183026d7745
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-01-25 09:09:41 +0200

    refactor: improve readability

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import "./index.css";
 import App from "../src/components/App";
-import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
